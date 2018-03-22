4888e041d3bb1acdac55752a2680694ac7215650
Adrienne Yao <adrienne.yao@icloud.com>
2018-03-22 20:14:57 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -8,7 +8,7 @@ class Owner
     @owner = owner
     @@all << self
     @species = "human"
-    @pets = {fish: [], cats: [], dogs: []}
+    @pets = {fishes: [], cats: [], dogs: []}
   end
 
   def species=(species)
