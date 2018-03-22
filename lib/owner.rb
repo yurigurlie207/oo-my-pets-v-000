d5c9fe15cd6814dc8d174339c34194c4869404cb
Adrienne Yao <adrienne.yao@icloud.com>
2018-03-22 21:41:54 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -82,7 +82,7 @@ class Owner
           dogcount = petArray.size
         end
 
-        puts "#{type"
+        puts "#{type}"
     }
     return "I have #{fishcount} fish, #{dogcount} dog(s), and #{catcount} cat(s)."
   end
